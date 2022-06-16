d375f0a6cfd53b8d66ce23e9c84884fec1f9576e
jot-jt <91110564+jot-jt@users.noreply.github.com>
2022-06-15 20:23:46 -0400

    Add persistent storage for quiz questions

--- cogs/quiz.py
+++ cogs/quiz.py
@@ -22,25 +22,58 @@ class Quiz(commands.Cog):
 
     @commands.command(help='Asks what the hiragana character is in romaji.')
     async def question(self, ctx):
+        # load player data from json
+        with open('data/users.json', 'r', encoding='utf-8') as f:
+            users = json.load(f)
+        print(users)
+        try:
+            player_data = users[str(ctx.author.id)]
+        except KeyError:
+            users[str(ctx.author.id)] = {
+                "correct_ct": 0,
+                "level": 1,
+                "familiarities": {
+                    "0": [],
+                    "1": [],
+                    "2": [],
+                    "3": [],
+                    "4": [],
+                    "5": []
+                }
+            }
+            player_data = users[ctx.author.id]
+
+        # load random hiragana from json
         with open('data/kana.json', 'r', encoding='utf-8') as f:
-            data = json.load(f)
-            hiragana = data['hiragana']
-        char, romaji = random.choice(list(hiragana.items()))
+            kana = json.load(f)
+        level = str(player_data["level"])
+        char_pool = kana['hiragana'][level]
+        jp_char, romaji = random.choice(list(char_pool.items()))
 
-        await ctx.send(f'What is the following character in romaji? {char}')
+        # generate question
+        await ctx.send(f'What is the following character in romaji? \n {jp_char}')
 
+        # check player response
         def check(msg):
             return ctx.author == msg.author and ctx.channel == msg.channel
 
         try:
             msg = await self.client.wait_for("message", timeout=10, check=check)
         except asyncio.TimeoutError:
-            await ctx.send(f'Time\'s up! The answer is {romaji}')
+            await ctx.send(f':hourglass: Time\'s up! The answer is `{romaji}`')
+            player_data["familiarities"]["0"].append(jp_char)
         else:
             if msg.content == romaji:
-                await ctx.send(f'Correct!')
+                await ctx.send(f':white_check_mark: Correct!')
+                player_data["correct_ct"] += 1
+                player_data["familiarities"]["1"].append(jp_char)
             else:
-                await ctx.send(f'Incorrect! The answer is {romaji}')
+                await ctx.send(f':x: Incorrect! The answer is `{romaji}`')
+                player_data["familiarities"]["0"].append(jp_char)
+
+        # write player data to json
+        with open('data/users.json', 'w', encoding='utf-8') as f:
+            users = json.dump(users, f, indent=4)
 
 
 def setup(client):
