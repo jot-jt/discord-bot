63df807d385b782d2d2e002f20b4cf60d88b930f
jot-jt <91110564+jot-jt@users.noreply.github.com>
2022-06-16 15:38:13 -0400

    Optimize quiz word selection

--- cogs/quiz.py
+++ cogs/quiz.py
@@ -14,16 +14,8 @@ class Quiz(commands.Cog):
         self.client = client
         self.in_progress = []
 
-    @commands.command(help='Prints a random hiragana character.')
-    async def hiragana(self, ctx):
-        with open('data/kana.json', 'r', encoding='utf-8') as f:
-            data = json.load(f)
-            hiragana = data['hiragana']
-        response = random.choice(list(hiragana.keys()))
-        await ctx.send(response)
-
-    @commands.command(aliases=['q'], help='Asks what the hiragana character is in romaji.')
-    async def question(self, ctx):
+    @commands.command(aliases=['q'], help='Asks what a Japanese character is in romaji.')
+    async def quiz(self, ctx):
         player_id = str(ctx.author.id)
 
         if player_id in self.in_progress:
@@ -40,9 +32,9 @@ class Quiz(commands.Cog):
             player_data = users[player_id]
         except KeyError:
             # create new profile
-            with open('data/kana.json', 'r', encoding='utf-8') as f:
+            with open('data/levels.json', 'r', encoding='utf-8') as f:
                 kana = json.load(f)
-            lvl_1_kana = list(kana['hiragana']['1'].keys())
+            lvl_1_kana = list(kana['1'].keys())
             users[str(ctx.author.id)] = {
                 "num_correct": 0,
                 "times_played": 0,
@@ -53,35 +45,73 @@ class Quiz(commands.Cog):
                     "2": [],
                     "3": [],
                     "4": [],
-                    "5": []
+                    "5": [],
+                    "6": [],
+                    "7": [],
+                    "8": [],
+                    "9": []
                 }
             }
             player_data = users[player_id]
 
-        # load hiragana pool from json to use for question
-        level = player_data["level"]
-        char_pool = []
-        char_weights = []  # weight for each entry in char_pool
-        bin_weights = np.array([10, 9, 6, 4, 4, 1])  # weight per frequency bin
-        for i in range(6):
-            char_pool += player_data['familiarities'][str(i)]
-            list_sz = len(player_data['familiarities'][str(i)])
-            char_weights += [bin_weights[i]] * list_sz
-        char_weights = char_weights / np.sum(char_weights)
-        jp_char = np.random.choice(
-            char_pool, replace=False, p=char_weights)
-        with open('data/kana.json', 'r', encoding='utf-8') as f:
-            kana = json.load(f)
-        for i in range(1, level+1):
-            romaji = kana['hiragana'][str(i)].get(jp_char)
-            if romaji != None:
-                break
+        def gen_question_data(player_data, bin_weights):
+            """
+            Loads a Q&A pair from levels.json based on player level
+            and word familiarity.
 
-        # generate question
+            Arguments:
+                player_data: Dictionary that represents a json entry
+                    in data/users.json
+                bin_weights: Normalized numpy array of size 10 that represents
+                    the probability distribution of each familiarity level
+            Returns:
+                A pair in the form of (question_word, answer)
+            Raises:
+                RuntimeError if no valid pair is found
+            """
+            found = False
+            rng = np.random.default_rng()
+            bin_priority = rng.choice(
+                np.arange(10), size=10, replace=False, p=bin_weights)
+            i = 0
+            while not found and i < 10:
+                bin_index = bin_priority[i]
+                bin = player_data['familiarities'][str(bin_index)]
+                bin_sz = len(bin)
+                if bin_sz > 0:
+                    jp_char = random.choice(bin)
+                    found = True
+                else:
+                    i += 1
+
+            with open('data/levels.json', 'r', encoding='utf-8') as f:
+                kana = json.load(f)
+            player_level = player_data["level"]
+            for i in range(1, player_level+1):
+                romaji = kana[str(i)].get(jp_char)
+                if romaji != None:
+                    return jp_char, romaji  # should always return
+
+            raise RuntimeError('Failed to locate Q&A pair')
+
+        # weight per frequency bin
+        bin_weights = np.array([15, 14, 10, 5, 3, 1, 1, 1, 1, 1])
+        bin_weights = bin_weights / np.sum(bin_weights)
+        jp_char, romaji = gen_question_data(player_data, bin_weights)
+
+        # print question
         await ctx.send(f'What is the following character in romaji? \n {jp_char}')
 
-        # check player response
         def check(msg):
+            """ 
+            Checks if message belongs to the same player and channel where the
+            command is called.
+
+            Inputs:
+                msg: discord.Message object
+            Returns:
+                bool
+            """
             return ctx.author == msg.author and ctx.channel == msg.channel
 
         correct = False
@@ -91,7 +121,7 @@ class Quiz(commands.Cog):
             await ctx.send(f':hourglass: Time\'s up! The answer is `{romaji}`')
             player_data["familiarities"]["0"].append(jp_char)
         else:
-            if msg.content == romaji:
+            if msg.content.casefold() == romaji.casefold():
                 await ctx.send(f':white_check_mark: Correct!')
                 player_data["num_correct"] += 1
                 correct = True
@@ -100,13 +130,13 @@ class Quiz(commands.Cog):
 
         # update data based on player response
         familiarity_lvl = 0
-        for i in range(6):
+        for i in range(10):
             if jp_char in player_data["familiarities"][str(i)]:
                 player_data["familiarities"][str(i)].remove(jp_char)
                 familiarity_lvl = i
 
         if correct:  # update familiarity lvl
-            familiarity_lvl = min(familiarity_lvl + 1, 5)  # check ceiling
+            familiarity_lvl = min(familiarity_lvl + 1, 9)  # check ceiling
         else:
             familiarity_lvl = max(familiarity_lvl - 1, 0)  # check floor
 
@@ -114,8 +144,11 @@ class Quiz(commands.Cog):
             player_data["familiarities"][str(familiarity_lvl)].append(jp_char)
 
         # check if player can level up based on if all words in current level are mastered
+        level = player_data["level"]
+        with open('data/levels.json', 'r', encoding='utf-8') as f:
+            kana = json.load(f)
         found_unmastered = False
-        for jp_char in kana['hiragana'][str(level)]:
+        for jp_char in kana[str(level)]:
             if jp_char not in player_data['familiarities']['5']:
                 found_unmastered = True
                 break
@@ -123,7 +156,7 @@ class Quiz(commands.Cog):
         if not found_unmastered:
             level += 1
             player_data["level"] = level
-            new_kana = kana['hiragana'][str(level)].keys()
+            new_kana = kana[str(level)].keys()
             player_data['familiarities']["0"] += new_kana
             await ctx.send(f':partying_face: Congratulations! You are now Level {level}!')
 
