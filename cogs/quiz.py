61de194f6e2b5882c540878ee84e7174666ffdb1
jot-jt <91110564+jot-jt@users.noreply.github.com>
2022-06-18 00:21:08 -0400

    Add profile command

--- cogs/quiz.py
+++ cogs/quiz.py
@@ -7,6 +7,11 @@ import random
 import asyncio
 import numpy as np
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+PROFILE_THUMBNAIL = os.getenv('PROFILE_THUMBNAIL')
+
 
 class Quiz(commands.Cog):
 
@@ -14,49 +19,58 @@ class Quiz(commands.Cog):
         self.client = client
         self.in_progress = []
 
-    @commands.command(aliases=['q'], help='Asks what a Japanese character is in romaji.')
-    async def quiz(self, ctx):
-        def load_player(player_id):
+    def load_player(self, player_id):
+        """
+        Loads player data from data/users.json. Creates a new profile for the
+        player if one does not exist.
+
+        Arguments:
+            player_id: Integer representation of discord user
+        Returns:
+            Python representation of the player's profile from users.json
+        """
+        def create_profile(player_id):
             """
-            Loads player data from data/users.json
+            Returns a new profile for player_id.
 
             Arguments:
                 player_id: Integer representation of discord user
             Returns:
-                all_users: Representation of entire users.json file
-                player_id: Representation of specific user in users.json
+                new profile representation for player_id
             """
-            player_id = str(player_id)
-
-            with open('data/users.json', 'r', encoding='utf-8') as f:
-                all_users = json.load(f)
-
-            try:
-                player_data = all_users[player_id]
-            except KeyError:  # create new profile
-                with open('data/levels.json', 'r', encoding='utf-8') as f:
-                    kana = json.load(f)
-                lvl_1_kana = list(kana['1'].keys())
-                all_users[str(ctx.author.id)] = {
-                    "num_correct": 0,
-                    "times_played": 0,
-                    "level": 1,
-                    "familiarities": {
-                        "0": lvl_1_kana,
-                        "1": [],
-                        "2": [],
-                        "3": [],
-                        "4": [],
-                        "5": [],
-                        "6": [],
-                        "7": [],
-                        "8": [],
-                        "9": []
-                    }
+            with open('data/levels.json', 'r', encoding='utf-8') as f:
+                kana = json.load(f)
+            lvl_1_kana = list(kana['1'].keys())
+            return {
+                "num_correct": 0,
+                "times_played": 0,
+                "level": 1,
+                "discovered": len(lvl_1_kana),
+                "familiarities": {
+                    "0": lvl_1_kana,
+                    "1": [],
+                    "2": [],
+                    "3": [],
+                    "4": [],
+                    "5": [],
+                    "6": [],
+                    "7": [],
+                    "8": [],
+                    "9": []
                 }
-                player_data = all_users[player_id]
-            return all_users, player_data
+            }
 
+        with open('data/users.json', 'r', encoding='utf-8') as f:
+            all_users = json.load(f)
+
+        try:
+            player_data = all_users[str(player_id)]
+        except KeyError:
+            player_data = create_profile(player_id)
+        return player_data
+
+    @commands.command(aliases=['q'], help='Asks what a Japanese character is in romaji.')
+    async def quiz(self, ctx):
         def gen_question_data(player_data, bin_weights):
             """
             Loads a Q&A pair from levels.json based on player level
@@ -109,7 +123,7 @@ class Quiz(commands.Cog):
             def check(msg):
                 return ctx.author == msg.author and ctx.channel == msg.channel
             try:
-                msg = await self.client.wait_for("message", timeout=10, check=check)
+                msg = await self.client.wait_for("message", timeout=5, check=check)
             except asyncio.TimeoutError:
                 await ctx.send(f':hourglass: Time\'s up! The answer is `{romaji}`')
                 player_data["familiarities"]["0"].append(jp_char)
@@ -164,6 +178,7 @@ class Quiz(commands.Cog):
                 player_data["level"] = level
                 new_kana = kana[str(level)].keys()
                 player_data['familiarities']["0"] += new_kana
+                player_data['discovered_vocab'] += len(kana[str(level)])
                 await ctx.send(f':partying_face: Congratulations! You are now Level {level}!')
 
         if ctx.author.id in self.in_progress:
@@ -171,9 +186,9 @@ class Quiz(commands.Cog):
             return
 
         self.in_progress.append(ctx.author.id)
-        all_users, player_data = load_player(ctx.author.id)
+        player_data = self.load_player(ctx.author.id)
 
-        bin_weights = np.array([20, 18, 18, 15, 10, 6, 5, 4, 3, 1])
+        bin_weights = np.array([18, 14, 14, 14, 12, 7, 7, 6, 5, 3])
         bin_weights = bin_weights / np.sum(bin_weights)
         jp_char, romaji = gen_question_data(player_data, bin_weights)
 
@@ -184,9 +199,55 @@ class Quiz(commands.Cog):
         player_data["times_played"] += 1
 
         # write player data to json
+        with open('data/users.json', 'r', encoding='utf-8') as f:
+            all_users = json.load(f)
+            all_users[str(ctx.author.id)] = player_data
         with open('data/users.json', 'w', encoding='utf-8') as f:
             json.dump(all_users, f, ensure_ascii=False, indent=4)
         self.in_progress.remove(ctx.author.id)
+
+    @commands.command(aliases=['pr'])
+    async def profile(self, ctx):
+        player_data = self.load_player(ctx.author.id)
+
+        color = discord.Color.dark_magenta().value
+        level = player_data['level']
+        num_correct = player_data['num_correct']
+        vocab_discovered = player_data['discovered_vocab']
+        times_played = player_data['times_played']
+
+        with open('data/levels.json', 'r', encoding='utf-8') as f:
+            total_vocab = json.load(f)['total_vocab']
+
+        profile = discord.Embed(
+            color=color
+        )
+
+        profile.set_author(name=ctx.author.display_name,
+                           icon_url=ctx.author.avatar_url)
+        profile.set_thumbnail(url=PROFILE_THUMBNAIL)
+        profile.add_field(name='Level', value=level, inline=False)
+        profile.add_field(
+            name='Correct Answers', value=num_correct, inline=False)
+        profile.add_field(
+            name='Times Played', value=times_played, inline=False)
+        profile.add_field(
+            name='Vocabulary Discovered', value=f'{vocab_discovered}/{total_vocab}', inline=False)
+        await ctx.send(embed=profile)
+
+    @commands.command()
+    @commands.is_owner()
+    async def updatevocabcount(self, ctx):
+        with open('data/levels.json', 'r', encoding='utf-8') as f:
+            levels = json.load(f)
+        total_lvls = levels['total_lvls']
+        sum = 0
+        for i in range(1, total_lvls + 1):
+            sum += len(levels[str(i)])
+        levels['total_vocab'] = sum
+        with open('data/levels.json', 'w', encoding='utf-8') as f:
+            json.dump(levels, f, ensure_ascii=False, indent=4)
+        await ctx.send(f'Update successful! There are {sum} vocabulary words.')
 
 
 def setup(client):
